9df4b3dcd9467996ab3030f55d284c6179916801
merakiStudiosKalopsia <merakistudioskalopsia@gmail.com>
2024-02-22 15:35:07 +0300

    Search working

--- services/api.ts
+++ services/api.ts
@@ -7,17 +7,19 @@ export const getTrending = async (): Promise<TrendingResult> => {
     `https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=e990df45b8f6e2a14d54e16e1c70e993&page1`
   );
   const json = await response.json();
+  console.log("🚀 ~ getTrending ~ json:", json)
 
   return json;
 };
 
 export const getSearchResults = async (query: string): Promise<TrendingResult> => {
   const response = await fetch(
-    `https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=${API_KEY}&query=${encodeURIComponent(
+    `https://api.themoviedb.org/3/search/multi?language=en-US&api_key=e990df45b8f6e2a14d54e16e1c70e993&query=${encodeURIComponent(
       query
     )}`
   );
   const data = await response.json();
+  console.log("🚀 ~ getSearchResults ~ data:", data)
   return data;
 };
 
